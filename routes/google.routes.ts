be979c4ff8b79dc70a784cad9fdf2bc6bf0a8abd
Ignacio <belmonteperona@gmail.com>
2020-05-09 06:05:51 +0200

    google router finished

--- routes/google.routes.ts
+++ routes/google.routes.ts
@@ -6,12 +6,23 @@ import { Order } from '../models/order.model';
 import { config } from '../config'
 const googleRoutes = Router();
 
-googleRoutes.post('/auth',async (req: any, res: Response, next: NextFunction) => {
-    if(req.body.uid && req.body.mail){
-        res.redirect(307,'/user/create')
-    }else{
+googleRoutes.post('/auth', async (req: any, res: Response, next: NextFunction) => {
+    const uid = req.body.uid;
+    if (req.body.uid && req.body.mail) {
+        let user
+        try {
+            user = await User.findOne({ uid: req.body.uid }).exec();
+        } catch (error) {
+
+        }
+        if (user) {
+            res.redirect(307, '/user/login')
+        } else {
+            res.redirect(307, '/user/create')
+        }
+    } else {
         return res.status(400).json({
-            ok:false,
+            ok: false,
             error: 'Bad request'
         })
     }
